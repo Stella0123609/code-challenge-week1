cc027e3b7f392ef35a4cbb03add14f65c5a54da6
stella njau <smaries6016@gmail.com>
2025-04-14 11:45:57 +0300

    Updates

---
+++ precache-manifest.d98ab2ce9570dc19e3fc91efd71a8e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5a83f53678d5bd450dbad5abe165dc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "de82e65dda02f9222110",
+    "url": "/my-app/static/css/main.1e87fccc.chunk.css"
+  },
+  {
+    "revision": "da8b8ee81106c8324f14",
+    "url": "/my-app/static/js/2.4519393a.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/my-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "de82e65dda02f9222110",
+    "url": "/my-app/static/js/main.6ec4266c.chunk.js"
+  },
+  {
+    "revision": "330a69b442416f742e8c",
+    "url": "/my-app/static/js/runtime~main.148f857f.js"
+  }
+]);
